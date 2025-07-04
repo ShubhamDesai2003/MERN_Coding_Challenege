0f4e92763e7f18f93b464a6413625b11705cc129
Shubham <92636412+ShubhamDesai2003@users.noreply.github.com>
2025-07-04 12:49:37 +0530

    Backend initial phase

--- Backend/routes/transactions.js
+++ Backend/routes/transactions.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const axios = require('axios');
-const Transaction = require('./transaction');
+const Transaction = require('../../models/transaction');
 
 const router = express.Router();
 
--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const transactionRoutes = require('./transactions');
+const transactionRoutes = require('./routes/transactions');
 
 const app = express();
 const PORT = 3000;
